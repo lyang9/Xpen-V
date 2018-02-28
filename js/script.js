c797f412e39c425a6e047ebcc25696fa1f1d5ef5
lyang9 <lyang9@buffalo.edu>
2018-02-27 22:48:04 -0500

    First commit to repository

--- js/script.js
+++ js/script.js
@@ -16,3 +16,17 @@
     }
   }
 })();
+
+
+var scale = Math.min(
+  availableWidth / contentWidth,
+  availableHeight / contentHeight
+);
+
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
